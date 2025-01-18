97cdd3406b77f84bed2448330e530e00649d8016
PixelPioneer2003 <154107788+PixelPioneer2003@users.noreply.github.com>
2025-01-18 12:13:23 +0530

    Update config.js

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,3 +1,3 @@
 // export const BASE_URL = 'http://localhost:3000/api/v1'
 // export const BASE_URL = 'http://mern-travel-booking-api.vercel.app/'
-export const BASE_URL='https://travel-world-t51m.onrender.com'
+export const BASE_URL='https://travel-world-t51m.onrender.com/api/v1'
